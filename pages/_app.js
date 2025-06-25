b033445bb65498dd76ce3b434789392d58179eeb
Ricardo Antonio Gómez Sotomayor <ricardo.gomez@tecsup.edu.pe>
2025-06-24 23:40:13 -0500

    Primer commit - proyecto SEO Next.js v5

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,18 @@
+import { Roboto } from "next/font/google";
 import Navbar from "../components/Navbar";
-import "../styles/globals.css"; // o Tailwind CSS si lo usas
+import "../styles/globals.css";
+
+// Carga la fuente con los estilos que necesitas
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["400", "700"], // puedes ajustar según lo que necesites
+});
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
+    <div className={roboto.className}>
       <Navbar />
       <Component {...pageProps} />
-    </>
+    </div>
   );
 }
